5c9f23bd80162566219ff089fbb5748fa8bfa108
sbtjavaschool <sbt.java.school@gmail.com>
2016-08-06 10:52:04 +0300

    reflection

---
+++ src/ru/sbt/reflection/domain/Person.java
@@ -0,0 +1,23 @@
+package ru.sbt.reflection.domain;
+
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private void sayName() {
+        System.out.println(name);
+    }
+}
